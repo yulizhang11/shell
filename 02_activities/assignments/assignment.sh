eebb06894cff7e2eeb0be3f242f1f693b6f19796
Yuli Zhang <yulizhang11@users.nonreply.github.com>
2025-11-07 21:17:26 -0500

    correct Q8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -58,9 +58,9 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 # ls ./data/processed/*/ >> ./data/inventory.txt
-ls ./data/processed/event_logs/ > ./data/inventory_1.txt
-ls ./data/processed/server_logs/ >> ./data/inventory_1.txt
-ls ./data/processed/user_logs/ >> ./data/inventory_1.txt
+ls ./data/processed/event_logs/ > ./data/inventory.txt
+ls ./data/processed/server_logs/ >> ./data/inventory.txt
+ls ./data/processed/user_logs/ >> ./data/inventory.txt
 
 
 ###########################################
